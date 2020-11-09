42c734efaebe32ba584ccf1459885861bbd226c5
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-11-09 15:34:31 -0500

    Added ability to add a list item

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+function newItem() {
+  
+  //adding a list item
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
